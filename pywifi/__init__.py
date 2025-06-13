98871ad6ad818f5238cef763c5a3153b769b040a
lbrendelsignum <lbrendel@signumiot.com>
2025-06-13 06:21:44 -0500

    fix: removing logging setup

--- pywifi/__init__.py
+++ pywifi/__init__.py
@@ -8,20 +8,8 @@ pywifi - a cross-platform wifi library.
 This library is made for manipulating wifi device on varient platforms.
 """
 
-import logging
-
 from pywifi import const
 from pywifi.profile import Profile
 from pywifi.wifi import PyWiFi
 
 __all__ = ["Profile", "PyWiFi"]
-
-def set_loglevel(level=logging.NOTSET):
-
-    format_pattern = "%(name)s %(asctime)s %(levelname)s %(message)s"
-    logging.basicConfig(format=format_pattern)
-    logger = logging.getLogger('pywifi')
-    logger.setLevel(level)
-
-
-set_loglevel()
